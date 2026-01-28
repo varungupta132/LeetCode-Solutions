9e1dc378323970d0072e912c256e50c97397ad17
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-01-28 16:20:04 +0530

    Time: 1 ms (98.13%) | Memory: 46.7 MB (6.56%) - LeetSync

---
+++ 3415-check-if-grid-satisfies-conditions/check-if-grid-satisfies-conditions.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean satisfiesConditions(int[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+
+                // vertical check
+                if (i + 1 < grid.length) {
+                    if (grid[i][j] != grid[i + 1][j]) {
+                        return false;
+                    }
+                }
+
+                // horizontal check
+                if (j + 1 < grid[0].length) {
+                    if (grid[i][j] == grid[i][j + 1]) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
