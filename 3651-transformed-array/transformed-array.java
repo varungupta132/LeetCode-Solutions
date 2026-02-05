686d718c1941e8ae5b441ed87b35ace6235cf993
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-02-05 23:50:23 +0530

    Time: 1 ms (98.72%) | Memory: 46.5 MB (86.54%) - LeetSync

--- 3651-transformed-array/transformed-array.java
+++ 3651-transformed-array/transformed-array.java
@@ -1,23 +1,10 @@
 class Solution {
     public int[] constructTransformedArray(int[] nums) {
-        int[] res = new int[nums.length];
-        for(int i = 0 ; i < nums.length ; i++){
-            int t = i;
-            if(nums[i] > 0 ){
-                t = (t + nums[i])%nums.length;
-                res[i] = nums[t];
-            }
-            else if(nums[i]<0){
-                int curr=(i+nums[i])%nums.length;
-                if(curr<0){
-                    curr+= nums.length;
-                }
-                res[i]=nums[curr];
-            }
-            else if(nums[i] == 0){
-                res[i] = nums[i];
-            }
-        }
-        return res;
-    }
+		int n = nums.length;
+		int[] ans = new int[n];
+		for (int i = 0; i < n; i++) {
+			ans[i] = nums[((i + nums[i]) % n + n) % n];
+		}
+		return ans;
+	}
 }
