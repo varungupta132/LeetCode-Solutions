57bd65233deba781693c4e500bed2d08acc9f192
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-03-16 22:47:46 +0530

    Time: 1 ms (100.00%) | Memory: 42.9 MB (0.00%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -1,18 +1,11 @@
 class Solution {
     public long countCommas(long n) {
-        long commas = 0;
-        long start = 1000;
-        int commaCount = 1;
-
-        while (start <= n) {
-            long end = start * 1000 - 1;
-            long numbers = Math.min(n, end) - start + 1;
-            commas += numbers * commaCount;
-
-            start *= 1000;
-            commaCount++;
+        long thres = 1000;
+        long ans = 0;
+        while(thres <= n){
+            ans += n-thres +1;
+            thres *= 1000;
         }
-
-        return commas;
+        return ans;
     }
 }
