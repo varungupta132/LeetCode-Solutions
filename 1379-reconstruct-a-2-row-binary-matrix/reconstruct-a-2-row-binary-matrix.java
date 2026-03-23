ffea9864deb6926f92bfadb7d31380e35f64ff43
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-03-23 23:31:52 +0530

    Time: 5 ms (100.00%) | Memory: 98.9 MB (17.96%) - LeetSync

--- 1379-reconstruct-a-2-row-binary-matrix/reconstruct-a-2-row-binary-matrix.java
+++ 1379-reconstruct-a-2-row-binary-matrix/reconstruct-a-2-row-binary-matrix.java
@@ -1,47 +1,48 @@
-import java.util.*;
-
-public class Solution {
+class Solution {
     public List<List<Integer>> reconstructMatrix(int upper, int lower, int[] colsum) {
-        int n = colsum.length;
-
         int sum = 0;
-        for(int x : colsum) sum += x;
+        for(int i : colsum){
+            sum += i;
+        }
+        if(upper+lower != sum) return new ArrayList<>();
+        int[] top = new int[colsum.length];
+        int[] down = new int[colsum.length];
 
-        if(sum != upper + lower) return new ArrayList<>();
-
-        int[] top = new int[n];
-        int[] bottom = new int[n];
-
-        // Step 1: fill colsum = 2
-        for(int i = 0; i < n; i++){
-            if(colsum[i] == 2){
-                top[i] = 1;
-                bottom[i] = 1;
+        for(int i = 0 ; i < colsum.length ; i++){
+            if(colsum[i] == 2 ){
+                top[i] = 1 ;
+                down[i] = 1;
                 upper--;
                 lower--;
             }
         }
 
-        // Step 2: fill colsum = 1
-        for(int i = 0; i < n; i++){
-            if(colsum[i] == 1){
-                if(upper > 0){
-                    top[i] = 1;
-                    upper--;
-                } else {
-                    bottom[i] = 1;
-                    lower--;
+
+            for(int i = 0 ; i < colsum.length ; i++){
+            if(colsum[i] == 1 ){
+                    if(upper>0){
+                        top[i] = 1;
+                        upper--;
+                    }else{
+                        down[i] = 1;
+                        lower--;
+                    }
                 }
-            }
         }
 
-        // final check
         if(upper != 0 || lower != 0) return new ArrayList<>();
 
-        List<List<Integer>> result = new ArrayList<>();
-        result.add(Arrays.asList(Arrays.stream(top).boxed().toArray(Integer[]::new)));
-        result.add(Arrays.asList(Arrays.stream(bottom).boxed().toArray(Integer[]::new)));
-
-        return result;
+        List<List<Integer>> aaa = new ArrayList<>();
+            ArrayList<Integer> a1 = new ArrayList<>();
+            ArrayList<Integer> a2 = new ArrayList<>();
+            for(int i : top){
+                a1.add(i);
+            }
+            for(int i : down){
+                a2.add(i);
+            }
+            aaa.add(a1);
+            aaa.add(a2);
+    return aaa;
     }
 }
