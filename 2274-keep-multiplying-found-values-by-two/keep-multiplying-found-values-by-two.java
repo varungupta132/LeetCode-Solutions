64f5f878c3d8f2da615cf5a138d0978f6596c32e
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-03-30 01:30:40 +0530

    Time: 8 ms (6.06%) | Memory: 46.5 MB (23.68%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
@@ -1,15 +1,11 @@
 class Solution {
-    public int findFinalValue(int[] nums, int ori) {
-        while(true){
-            boolean f = false;
-            for(int i = 0 ; i < nums.length ; i++){
-                if(nums[i] == ori ){
-                    f = true;
-                    break;
-                }
-            }
-            if(f) ori *= 2;
-            else return ori;
+    public int findFinalValue(int[] nums, int original) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(original==nums[i])
+                 original *=2;
         }
+        return original;
+        
     }
 }
