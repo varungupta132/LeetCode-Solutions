59e852984be413ea42d2812000850b54058b876b
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-04-01 23:24:06 +0530

    Time: 4 ms (16.61%) | Memory: 46.7 MB (14.18%) - LeetSync

--- 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.java
+++ 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.java
@@ -3,17 +3,6 @@ class Solution {
         StringBuilder sb = new StringBuilder(num);
         boolean st = false;
         boolean end = false;
-        for(int i = 0 ; i < sb.length() ; i++){
-            if(sb.charAt(i) == '0' && st == false){
-                sb.deleteCharAt(i);
-                i--;
-            }
-            else if(sb.charAt(i) != '0'){
-                st = true;
-                break;
-            }
-        }
-
         for(int i = sb.length() -1 ; i >=0  ; i--){
             if(sb.charAt(i) == '0' && end == false){
                 sb.deleteCharAt(i);
