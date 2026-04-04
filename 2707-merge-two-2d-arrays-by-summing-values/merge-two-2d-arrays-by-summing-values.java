cfbf7a3458198f9e49ac45444dcc4d1056a39344
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-04-05 02:00:29 +0530

    Time: 6 ms (8.76%) | Memory: 47.4 MB (9.44%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
@@ -1,24 +1,33 @@
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+        TreeMap<Integer , Integer> hm = new TreeMap<>();
 
         for(int[] arr : nums1){
-            map.put(arr[0], map.getOrDefault(arr[0], 0) + arr[1]);
+            hm.put(arr[0] , hm.getOrDefault(arr[0] , 0) + arr[1]);
         }
-
         for(int[] arr : nums2){
-            map.put(arr[0], map.getOrDefault(arr[0], 0) + arr[1]);
+            hm.put(arr[0] , hm.getOrDefault(arr[0] , 0) + arr[1]);
         }
-
-        int[][] result = new int[map.size()][2];
-        int i = 0;
-
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            result[i][0] = entry.getKey();
-            result[i][1] = entry.getValue();
-            i++;
+        ArrayList<ArrayList<Integer>> arrr = new ArrayList<>();
+        for(Map.Entry<Integer , Integer> entry : hm.entrySet()){
+            int key = entry.getKey();
+            int value = entry.getValue();
+            ArrayList<Integer> ar = new ArrayList<>();
+            ar.add(key);
+            ar.add(value);
+            arrr.add(ar);
         }
+        // Collections.sort(arrr, (a, b) -> a.get(0) - b.get(0));
+        int[][] result = new int[arrr.size()][2];
 
-        return result;
+for (int i = 0; i < arrr.size(); i++) {
+    result[i][0] = arrr.get(i).get(0);
+    result[i][1] = arrr.get(i).get(1);
+}
+
+return result;
+
+        
+
     }
 }
