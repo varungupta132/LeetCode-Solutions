4dd2062025636695c2c2a1ee14e33ba6a096cd00
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-04-06 12:37:02 +0530

    Time: 60 ms (5.34%) | Memory: 59.2 MB (10.60%) - LeetSync

--- 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.java
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.java
@@ -1,26 +1,36 @@
+import java.lang.reflect.Method;
+
 class Solution {
-    public String decodeCiphertext(String en, int rows) {
-        if(rows == 0) return "";
-        
+    public String decodeCiphertext(String en, int row) {
         int n = en.length();
-        int cols = n / rows;
-
+        int col = n/row;
+        char [][] dp = new char [row][col];
+        int idx = 0;
         StringBuilder sb = new StringBuilder();
-
-        // Traverse from each column of first row
-        for(int startCol = 0; startCol < cols; startCol++) {
-            int i = 0;
-            int j = startCol;
-
-            // diagonal traversal
-            while(i < rows && j < cols) {
-                sb.append(en.charAt(i * cols + j));
-                i++;
-                j++;
+        for(int i = 0 ; i <dp.length ; i++ ){
+            for(int j = 0 ; j < dp[0].length ; j++){
+                dp[i][j] = en.charAt(idx++);
             }
         }
 
-        // remove trailing spaces
-        return sb.toString().replaceAll("\\s+$", "");
+        for(int i = 0 ; i < dp[0].length ; i++){
+            
+           int r = 0, c = i;
+            while (r < row && c < col) {
+                sb.append(dp[r][c]);
+                r++;
+                c++;
+            }
+        }
+        Method[] methods = StringBuilder.class.getMethods();
+
+        for (Method m : methods) {
+            System.out.println(m.getName());
+        }
+        for(int i = sb.length()-1 ; i >=0 ; i--){
+            if(sb.charAt(i) == ' ') sb.deleteCharAt(i);
+            else break;
+        }
+        return sb.toString();
     }
 }
