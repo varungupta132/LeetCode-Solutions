b81320409f36f587bc9ab6ea0ac905ae345b4d2d
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-04-06 12:41:05 +0530

    Time: 26 ms (52.19%) | Memory: 56.6 MB (70.31%) - LeetSync

--- 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.java
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.java
@@ -1,36 +1,29 @@
-import java.lang.reflect.Method;
-
 class Solution {
     public String decodeCiphertext(String en, int row) {
-        int n = en.length();
-        int col = n/row;
-        char [][] dp = new char [row][col];
-        int idx = 0;
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0 ; i <dp.length ; i++ ){
-            for(int j = 0 ; j < dp[0].length ; j++){
-                dp[i][j] = en.charAt(idx++);
-            }
-        }
+        if (row == 0) return "";
 
-        for(int i = 0 ; i < dp[0].length ; i++){
-            
-           int r = 0, c = i;
+        int n = en.length();
+        int col = n / row;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < col; i++) {
+            int r = 0, c = i;
+
             while (r < row && c < col) {
-                sb.append(dp[r][c]);
+                int idx = r * col + c;
+                sb.append(en.charAt(idx));
                 r++;
                 c++;
             }
         }
-        Method[] methods = StringBuilder.class.getMethods();
 
-        for (Method m : methods) {
-            System.out.println(m.getName());
+        // remove trailing spaces
+        int end = sb.length() - 1;
+        while (end >= 0 && sb.charAt(end) == ' ') {
+            end--;
         }
-        for(int i = sb.length()-1 ; i >=0 ; i--){
-            if(sb.charAt(i) == ' ') sb.deleteCharAt(i);
-            else break;
-        }
-        return sb.toString();
+
+        return sb.substring(0, end + 1);
     }
 }
