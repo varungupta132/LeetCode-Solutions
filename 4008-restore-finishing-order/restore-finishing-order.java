40644c8d4dcf72120ab7e4e10a480f94bea6362a
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-04-08 01:12:22 +0530

    Time: 2 ms (52.23%) | Memory: 46.4 MB (96.34%) - LeetSync

--- 4008-restore-finishing-order/restore-finishing-order.java
+++ 4008-restore-finishing-order/restore-finishing-order.java
@@ -2,13 +2,24 @@ class Solution {
     public int[] recoverOrder(int[] or, int[] fri) {
         int[] ans = new int[fri.length];
         int idx = 0;
-        for(int i : or){
-            for(int j = 0 ; j < fri.length ; j++){
-                if(fri[j] == i){
-                    ans[idx++] = fri[j];
+
+        // Count frequencies of elements in fri
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int f : fri) {
+            map.put(f, map.getOrDefault(f, 0) + 1);
+        }
+
+        // Place elements according to or
+        for (int o : or) {
+            if (map.containsKey(o)) {
+                int count = map.get(o);
+                for (int i = 0; i < count; i++) {
+                    ans[idx++] = o;
                 }
+                map.remove(o); // optional, not needed
             }
         }
+
         return ans;
     }
 }
