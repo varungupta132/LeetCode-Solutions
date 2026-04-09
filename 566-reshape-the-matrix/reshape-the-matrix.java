f14d40ac3566a3c6c400cb4c35ed6e71377756b5
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-04-10 01:03:20 +0530

    Time: 1 ms (59.79%) | Memory: 47.4 MB (68.03%) - LeetSync

--- 566-reshape-the-matrix/reshape-the-matrix.java
+++ 566-reshape-the-matrix/reshape-the-matrix.java
@@ -1,30 +1,21 @@
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
-
-        int m = mat.length;
-        int n = mat[0].length;
-
-        // ✅ check possible or not
-        if (m * n != r * c) return mat;
-
         int[][] ans = new int[r][c];
-
+        if(r*c !=  mat.length * mat[0].length) return mat;
+        
         int ridx = 0;
-        int cidx = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-
-                ans[ridx][cidx] = mat[i][j];  // ✅ pehle fill karo
+        int cidx = 0 ;
+        for(int i = 0 ;i < mat.length ; i++){
+            for(int j = 0 ; j < mat[0].length ; j++){
+                // if(ridx == r-1) ridx=0;
+                ans[ridx][cidx] = mat[i][j];
                 cidx++;
-
-                if (cidx == c) {   // ✅ baad me move karo
-                    cidx = 0;
-                    ridx++;
+                if(cidx == c){ 
+                    ridx++ ;
+                    cidx = 0 ;
                 }
             }
         }
-
         return ans;
     }
 }
