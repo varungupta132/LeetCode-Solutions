36f5d905c3fbe4a2e309e6aa70348fa0b1c33d9d
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-04-25 15:58:44 +0530

    Time: 406 ms (41.01%) | Memory: 0B (100.00%) - LeetSync

--- 1670-patients-with-a-condition/patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/patients-with-a-condition.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 
-select patient_id , patient_name , conditions  from Patients where conditions like "DIAB1%"
-  OR conditions LIKE '% DIAB1%';
+-- select patient_id , patient_name , conditions  from Patients where conditions like "DIAB1%"
+--   OR conditions LIKE '% DIAB1%';
 
 
 -- SELECT patient_id, patient_name, conditions
@@ -12,6 +12,7 @@ select patient_id , patient_name , conditions  from Patients where conditions li
 
 # Write your MySQL query statement below
 
--- select patient_id , patient_name , conditions  from Patients where condition in (
---     select conditions fron Patients like "DIAB1"% 
--- ) 
+select patient_id , patient_name , conditions  from Patients where conditions in (
+    select conditions from Patients where conditions like "DIAB1%" 
+   OR conditions LIKE '% DIAB1%'
+) 
