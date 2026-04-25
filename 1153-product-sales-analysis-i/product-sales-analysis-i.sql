b203f96849bc630c8db0efd151eaaf7fc2a24738
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-04-25 15:35:00 +0530

    Time: 1135 ms (85.78%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,3 +1,17 @@
 # Write your MySQL query statement below
 
-select product_name , year , price from Sales left join Product on Sales.product_id = Product.product_id 
+-- select product_name , year , price from Sales full outer join Product on Sales.product_id = Product.product_id 
+
+
+
+SELECT product_name, year, price
+FROM Sales
+LEFT JOIN Product 
+ON Sales.product_id = Product.product_id
+
+-- UNION
+
+-- SELECT product_name, year, price
+-- FROM Sales
+-- RIGHT JOIN Product 
+-- ON Sales.product_id = Product.product_id;
