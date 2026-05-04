f8a917c3b6548fa770c91282726f49ff07f87b38
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-05-05 04:49:14 +0530

    Time: 1 ms (71.13%) | Memory: 43.8 MB (13.96%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,32 +1,8 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+
     public List<Integer> rightSideView(TreeNode root) {
+
         List<Integer> arr = new ArrayList<>();
-    //     addr(arr , root);
-    //     return arr;
-    // }
-    // public void addr(List<Integer> arr , TreeNode root){
-    //     if(root == null) return ;
-    //     arr.add(root.val);
-    //     addr(arr , root.right);
-    // }
-
-
-    List<List<Integer>> ans = new ArrayList<>();
 
         if(root == null) return arr;
 
@@ -38,13 +14,9 @@ class Solution {
 
             int n = q.size();
 
-            List<Integer> level = new ArrayList<>();
-
             for(int i = 0 ; i < n ; i++){
 
                 TreeNode curr = q.poll();
-
-                level.add(curr.val);
 
                 if(curr.left != null){
                     q.offer(curr.left);
@@ -53,15 +25,14 @@ class Solution {
                 if(curr.right != null){
                     q.offer(curr.right);
                 }
+
+                // last node of level
+                if(i == n - 1){
+                    arr.add(curr.val);
+                }
             }
-
-            ans.add(level);
         }
 
-        for(List<Integer> lst : ans){
-            arr.add(lst.get(lst.size()-1));
-        }
         return arr;
-
-}
+    }
 }
