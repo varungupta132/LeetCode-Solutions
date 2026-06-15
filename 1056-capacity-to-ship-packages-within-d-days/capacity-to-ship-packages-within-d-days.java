96f9c8450f7f65f79d9fceeed62230a3f0ec199a
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-06-15 13:36:23 +0530

    Time: 21 ms (5.44%) | Memory: 49.4 MB (99.01%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,45 +1,35 @@
 class Solution {
     public int shipWithinDays(int[] we, int da) {
-
-        int l = 0;
+        int l = 0 ;
         int r = 0;
-
-        for(int w : we){
-            l = Math.max(l, w);
-            r += w;
+        for(int i : we ){
+            l = Math.max(l , i);
+            r += i;
         }
-
-        while(l <= r){
-
-            int mid = l + (r - l) / 2;
-
-            if(check(mid, we, da)){
-                r = mid - 1;
+        while( l<= r){
+            int mid = l + (r-l)/2 ;
+            System.out.println(mid);
+            if(check(we , da , mid)){
+                r = mid -1;
             }
             else{
-                l = mid + 1;
+                l = mid+1;
             }
         }
-
         return l;
     }
 
-    public boolean check(int cap, int[] we, int da){
-
-        int days = 1;
-        int curr = 0;
-
-        for(int w : we){
-
-            if(curr + w > cap){
-                days++;
-                curr = w;
-            }
-            else{
-                curr += w;
+    public boolean check(int[] we , int da , int mid){
+        int c = 0 ;
+        int d = 1;
+        for(int i = 0 ; i < we.length ; i++){
+            if( c + we[i] <= mid){
+                c+=we[i];
+            }else{
+                c = we[i];
+                d++;
             }
         }
-
-        return days <= da;
+        return d <= da;
     }
 }
