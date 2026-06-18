d2248cc49e7af098453d4ece33aad68b16a34cae
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-06-18 09:54:07 +0530

    Time: 20 ms (33.54%) | Memory: 82.7 MB (45.22%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,39 +1,40 @@
 class Solution {
     public int minDays(int[] blo, int m, int k) {
-        long l = Integer.MIN_VALUE; 
-        long r = 0;
+        int l = Integer.MAX_VALUE ;
+        int r = Integer.MIN_VALUE ;
+
         for(int i : blo){
-            l=Math.min(l , (long)i);
-            r = Math.max(r , (long)i);
+            l = Math.min(l , i);
+            r = Math.max(r , i);
         }
-        long ans=-1;
-        while(l<=r){
-            long mid=l+(r-l)/2;
-            if(check(blo , mid , m , k)){
-                ans=mid;
-                r=mid-1;
-            }else{
-                l=mid+1;
+        int ans = -1 ;
+        while( l <= r){
+            int mid = l + (r-l)/2;
+            if(check(blo , k , m , mid)){
+                ans =mid ;
+                r = mid - 1;
+            }
+            else{
+                l = mid+1;
             }
         }
-        return (int)ans;
+        return ans;
     }
 
-    public boolean check(int[] blo , long ans , int m , int k){
-        int f=0;
-        int b=0;
-        for(int i = 0 ; i <blo.length;i++){
-          if(blo[i]<=ans){
-             f++;
-             if(f==k){
-                b++;
-                f=0;
-             }
-          }else{
-            f=0;
-          }
+    public boolean check(int[] blo , int k , int m , int ans){
+        int f = 0 ; 
+        int d = 0 ;
+        for(int i = 0 ; i < blo.length ; i++){
+            if(blo[i] <= ans){
+            f++;
+            if(f == k) {
+                f = 0 ;
+                d++;
+            }
+            }else{
+                f = 0;   
+            } 
         }
-        return b>=m?true:false;
-      
+        return d>=m;
     }
 }
