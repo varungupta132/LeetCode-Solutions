6fa95ba4d98701cf8866661c774fcbe51162da24
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-06-23 16:42:57 +0530

    Time: 11 ms (34.99%) | Memory: 44.7 MB (5.23%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -1,11 +1,35 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        int[] freq = new int[26];
-        for(char ch : text.toCharArray()){
-            freq[ch - 'a']++;
+        HashMap<Character, Integer> hm = new HashMap<>();
+
+        for (char ch : text.toCharArray()) {
+            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
         }
-        freq['l' - 'a'] /= 2;
-        freq['o' - 'a'] /= 2;  
-        return Math.min(freq['o' - 'a'] , Math.min(freq['b'- 'a'] , Math.min(freq['a'- 'a'] , Math.min(freq['l'- 'a'] , freq['n'- 'a']  ))) );
+
+        int c = 0;
+
+        while (true) {
+
+            // check if one more "balloon" can be formed
+            if (hm.getOrDefault('b', 0) >= 1 &&
+                hm.getOrDefault('a', 0) >= 1 &&
+                hm.getOrDefault('l', 0) >= 2 &&
+                hm.getOrDefault('o', 0) >= 2 &&
+                hm.getOrDefault('n', 0) >= 1) {
+
+                // remove characters of one balloon
+                hm.put('b', hm.get('b') - 1);   // corrected
+                hm.put('a', hm.get('a') - 1);   // corrected
+                hm.put('l', hm.get('l') - 2);   // corrected
+                hm.put('o', hm.get('o') - 2);   // corrected
+                hm.put('n', hm.get('n') - 1);   // corrected
+
+                c++;
+            } else {
+                break;
+            }
+        }
+
+        return c;
     }
 }
