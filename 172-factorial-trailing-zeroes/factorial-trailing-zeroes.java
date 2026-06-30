2abc5451063c10c699ccdc4f933e9ef194fa67ed
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-06-30 23:32:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (72.23%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
@@ -1,11 +1,10 @@
 class Solution {
     public int trailingZeroes(int n) {
-        int c  = 0 ;
-        // if(n == 0) return 0;
-        while(n > 0){
-            c = c + n / 5 ;
-            n = n / 5;
-        }
-        return c;
+     int c = 0;
+     while( n > 0){
+        n/=5;
+        c += (n);
+     }   
+     return c;
     }
 }
