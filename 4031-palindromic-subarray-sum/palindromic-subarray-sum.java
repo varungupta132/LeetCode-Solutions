93f915191903f868cf249acea97fdbd6011a429e
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-07-10 01:19:40 +0530

    Time: 16 ms (51.56%) | Memory: 142.5 MB (45.32%) - LeetSync

--- 4031-palindromic-subarray-sum/palindromic-subarray-sum.java
+++ 4031-palindromic-subarray-sum/palindromic-subarray-sum.java
@@ -1,54 +1,56 @@
 class Solution {
     public long getSum(int[] nums) {
-        return maxPalindromeSum(nums);
-    }
-    
-// }
-// class Solution {
-    public long maxPalindromeSum(int[] nums) {
         int n = nums.length;
 
-        long[] prefix = new long[n + 1];
-        for (int i = 0; i < n; i++)
-            prefix[i + 1] = prefix[i] + nums[i];
+        long[] pre = new long[n + 1];
+        for (int i = 0; i < n; i++) {
+            pre[i + 1] = pre[i] + nums[i];
+        }
 
         long ans = 0;
         for (int x : nums) ans = Math.max(ans, x);
 
-        // odd length palindrome
+        // Odd-length palindromes
         int[] d1 = new int[n];
         int l = 0, r = -1;
         for (int i = 0; i < n; i++) {
             int k = (i > r) ? 1 : Math.min(d1[l + r - i], r - i + 1);
-            while (i - k >= 0 && i + k < n && nums[i - k] == nums[i + k])
+
+            while (i - k >= 0 && i + k < n && nums[i - k] == nums[i + k]) {
                 k++;
+            }
+
             d1[i] = k;
 
             int left = i - k + 1;
             int right = i + k - 1;
-            ans = Math.max(ans, prefix[right + 1] - prefix[left]);
+            ans = Math.max(ans, pre[right + 1] - pre[left]);
 
-            if (i + k - 1 > r) {
-                l = i - k + 1;
-                r = i + k - 1;
+            if (right > r) {
+                l = left;
+                r = right;
             }
         }
 
-        // even length palindrome
+        // Even-length palindromes
         int[] d2 = new int[n];
         l = 0;
         r = -1;
+
         for (int i = 0; i < n; i++) {
             int k = (i > r) ? 0 : Math.min(d2[l + r - i + 1], r - i + 1);
+
             while (i - k - 1 >= 0 && i + k < n &&
-                    nums[i - k - 1] == nums[i + k])
+                    nums[i - k - 1] == nums[i + k]) {
                 k++;
+            }
+
             d2[i] = k;
 
             if (k > 0) {
                 int left = i - k;
                 int right = i + k - 1;
-                ans = Math.max(ans, prefix[right + 1] - prefix[left]);
+                ans = Math.max(ans, pre[right + 1] - pre[left]);
             }
 
             if (i + k - 1 > r) {
