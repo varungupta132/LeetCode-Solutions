01f875c1d1b6165d3c53948594341fd8b97c6201
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-07-10 01:18:24 +0530

    Time: 15 ms (62.05%) | Memory: 142.5 MB (37.87%) - LeetSync

---
+++ 4031-palindromic-subarray-sum/palindromic-subarray-sum.java
@@ -0,0 +1,62 @@
+class Solution {
+    public long getSum(int[] nums) {
+        return maxPalindromeSum(nums);
+    }
+    
+// }
+// class Solution {
+    public long maxPalindromeSum(int[] nums) {
+        int n = nums.length;
+
+        long[] prefix = new long[n + 1];
+        for (int i = 0; i < n; i++)
+            prefix[i + 1] = prefix[i] + nums[i];
+
+        long ans = 0;
+        for (int x : nums) ans = Math.max(ans, x);
+
+        // odd length palindrome
+        int[] d1 = new int[n];
+        int l = 0, r = -1;
+        for (int i = 0; i < n; i++) {
+            int k = (i > r) ? 1 : Math.min(d1[l + r - i], r - i + 1);
+            while (i - k >= 0 && i + k < n && nums[i - k] == nums[i + k])
+                k++;
+            d1[i] = k;
+
+            int left = i - k + 1;
+            int right = i + k - 1;
+            ans = Math.max(ans, prefix[right + 1] - prefix[left]);
+
+            if (i + k - 1 > r) {
+                l = i - k + 1;
+                r = i + k - 1;
+            }
+        }
+
+        // even length palindrome
+        int[] d2 = new int[n];
+        l = 0;
+        r = -1;
+        for (int i = 0; i < n; i++) {
+            int k = (i > r) ? 0 : Math.min(d2[l + r - i + 1], r - i + 1);
+            while (i - k - 1 >= 0 && i + k < n &&
+                    nums[i - k - 1] == nums[i + k])
+                k++;
+            d2[i] = k;
+
+            if (k > 0) {
+                int left = i - k;
+                int right = i + k - 1;
+                ans = Math.max(ans, prefix[right + 1] - prefix[left]);
+            }
+
+            if (i + k - 1 > r) {
+                l = i - k;
+                r = i + k - 1;
+            }
+        }
+
+        return ans;
+    }
+}
