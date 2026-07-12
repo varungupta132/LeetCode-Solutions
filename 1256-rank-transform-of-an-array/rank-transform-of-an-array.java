2b61e8f81d645c9d11c1027d62ba8cb2ec4a7ae5
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-07-12 18:58:31 +0530

    Time: 67 ms (7.35%) | Memory: 75.9 MB (50.49%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,21 +1,39 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        int[] sorted = arr.clone();
-Arrays.sort(sorted);
 
-HashMap<Integer, Integer> map = new HashMap<>();
-int rank = 1;
+        if (arr.length == 0) {
+            return new int[0];
+        }
 
-for(int num : sorted){
-    if(!map.containsKey(num)){
-        map.put(num, rank++);
-    }
-}
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-for(int i = 0; i < arr.length; i++){
-    arr[i] = map.get(arr[i]);
-}
+        for (int num : arr) {
+            pq.offer(num);
+        }
 
-return arr;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int rank = 1;
+        int prev = pq.poll();
+        map.put(prev, rank);
+
+        while (!pq.isEmpty()) {
+            int curr = pq.poll();
+
+            if (curr != prev) {
+                rank++;
+            }
+
+            map.put(curr, rank);
+            prev = curr;
+        }
+
+        int[] ans = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            ans[i] = map.get(arr[i]);
+        }
+
+        return ans;
     }
 }
