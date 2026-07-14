5c747dbac3df320036d7399d1757c320d3c45868
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-07-15 01:38:33 +0530

    Time: 10 ms (5.61%) | Memory: 46.7 MB (5.37%) - LeetSync

--- 2476-check-distances-between-same-letters/check-distances-between-same-letters.java
+++ 2476-check-distances-between-same-letters/check-distances-between-same-letters.java
@@ -1,9 +1,16 @@
 class Solution {
     public boolean checkDistances(String s, int[] distance) {
-        for(int i = 0 ; i < s.length() ;i++){
-            for(int j = i+1 ; j < s.length() ;j++){
-                if(s.charAt(i) == s.charAt(j) && distance[s.charAt(j) -'a'] != j-i-1) return false;
-            }
+        HashMap<Character, ArrayList<Integer>> hm = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            hm.putIfAbsent(ch, new ArrayList<>());
+            hm.get(ch).add(i);
+        }
+        System.out.println(hm);
+        for(char ch : hm.keySet()){
+            ArrayList<Integer> mm = hm.get(ch);
+            int dis = Math.abs(mm.get(0) - mm.get(1));
+            if(dis-1 != distance[ch-'a']) return false;
         }
         return true;
     }
