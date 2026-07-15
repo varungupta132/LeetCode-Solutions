440c598610f609d88c8d71e39f504c83c406f2d0
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-07-15 13:39:52 +0530

    Time: 23 ms (91.72%) | Memory: 73.1 MB (73.25%) - LeetSync

---
+++ 2305-append-k-integers-with-minimal-sum/append-k-integers-with-minimal-sum.java
@@ -0,0 +1,48 @@
+class Solution {
+    public long minimalKSum(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int x = 1;
+        long ans = 0;
+
+        for (int i : nums) {
+
+            if (i < x) continue;
+
+            if (i == x) {
+                x = i + 1;
+                continue;
+            }
+
+            int n = i - x;
+
+            if (k > n) {
+                k -= n;
+
+                long first = x;
+                long last = (long) i - 1;
+
+                ans += (long) n * (first + last) / 2;
+
+                x = i + 1;
+            } else {
+                long first = x;
+                long last = (long) x + k - 1;
+
+                ans += (long) k * (first + last) / 2;
+
+                k = 0;
+                break;
+            }
+        }
+
+        if (k > 0) {
+            long first = x;
+            long last = (long) x + k - 1;
+
+            ans += (long) k * (first + last) / 2;
+        }
+
+        return ans;
+    }
+}
