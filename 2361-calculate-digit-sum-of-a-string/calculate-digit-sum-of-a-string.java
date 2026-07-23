958855aa85bec13b2baf88afab256fd3303cdd32
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-07-23 20:54:19 +0530

    Time: 1 ms (98.59%) | Memory: 42.9 MB (62.82%) - LeetSync

--- 2361-calculate-digit-sum-of-a-string/calculate-digit-sum-of-a-string.java
+++ 2361-calculate-digit-sum-of-a-string/calculate-digit-sum-of-a-string.java
@@ -1,24 +1,27 @@
 class Solution {
     public String digitSum(String s, int k) {
-
-        while (s.length() > k) {
+        int idx = 0;
+        while(s.length() >k){
             StringBuilder sb = new StringBuilder();
-
-            for (int i = 0; i < s.length(); i += k) {
-                int end = Math.min(i + k, s.length());
-                sb.append(sum(s.substring(i, end)));
+            for(int i = 0 ; i < s.length();  i = i+k){
+                // int mini = Math.min(i+k,s.length());
+                String str = s.substring(i, Math.min(i+k , s.length()));
+                // System.out.println(str);
+                sb.append(sum(str));
+                // System.out.println(str);
             }
-
+            // sb.append(sum(s.substring(s.length()-(s.length()%k) , s.length())));
+            // System.out.println(sb);
             s = sb.toString();
+            // idx++;
         }
-
         return s;
-    }
 
-    public String sum(String str) {
+    }
+    public String sum(String str ){
         int sum = 0;
-        for (char ch : str.toCharArray()) {
-            sum += ch - '0';
+        for(char ch : str.toCharArray()){
+            sum+= ch-'0';
         }
         return String.valueOf(sum);
     }
