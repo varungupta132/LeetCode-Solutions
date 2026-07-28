6cdcc0a84c7df5465e2fd9b9c920617865240bce
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-07-28 18:18:52 +0530

    Time: 56 ms (35.40%) | Memory: 44.6 MB (41.47%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,41 +1,26 @@
 class Solution {
-    int[] dp ;
+    int[] dp;
+
     public int numSquares(int n) {
-        
-        // if( n == 4 || n == 9 || n == 16 || n ==1) return 1;
-        // if(n == 0) return 0;
-        // int ans = Integer.MAX_VALUE;
-        dp = new int[n+1];
-        Arrays.fill(dp , -1);
-
-
-        // for(int i = 1 ; i*i <= n ; i++){
-        //     if(dp[n-i*i] == -1){
-        //     dp[n-i*i] = solve(n-i*i);
-        //     } 
-        //     // else
-        //     ans = Math.min(1+dp[n-i*i] , ans);
-        // }
+        dp = new int[n + 1];
+        Arrays.fill(dp, -1);
         return solve(n);
     }
-    int solve(int n ){
 
-        if( n == 4 || n == 9 || n == 16 || n ==1) return 1;
-        if(n == 0) return 0;
-        if(dp[n] != -1){
+    int solve(int n) {
+
+        if (n == 0)
+            return 0;
+
+        if (dp[n] != -1)
             return dp[n];
-        }
+
         int ans = Integer.MAX_VALUE;
 
-        for(int i = 1 ; i*i <= n ; i++){
-            if(dp[n-i*i] == -1){
-             dp[n-i*i] = solve(n-i*i);
-            } 
-            // else
-            ans = Math.min(1+dp[n-i*i] , ans);
-            
+        for (int i = 1; i * i <= n; i++) {
+            ans = Math.min(ans, 1 + solve(n - i * i));
         }
+
         return dp[n] = ans;
-        
     }
 }
