6f1f56de2885a6f0cc4590168d24f7daedaf21b0
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-08-08 04:04:33 +0530

    Time: 1 ms (95.86%) | Memory: 46.8 MB (62.62%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -1,38 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
+        
+        Queue<TreeNode> q = new ArrayDeque<>();
+        List<List<Integer>> lsst = new ArrayList<>();
+        if(root == null) return lsst;
 
-        List<List<Integer>> ans = new ArrayList<>();
-
-        if(root == null) return ans;
-
-        Queue<TreeNode> q = new LinkedList<>();
-
-        q.offer(root);
-
+        q.add(root);
         while(!q.isEmpty()){
-
+            List<Integer> lst = new ArrayList<Integer>();
             int n = q.size();
-
-            List<Integer> level = new ArrayList<>();
-
             for(int i = 0 ; i < n ; i++){
-
-                TreeNode curr = q.poll();
-
-                level.add(curr.val);
-
-                if(curr.left != null){
-                    q.offer(curr.left);
+                TreeNode x = q.poll();
+                lst.add(x.val);
+                if(x.left != null){
+                    q.add(x.left);   }
+                if(x.right != null){
+                    q.add(x.right);  }
                 }
-
-                if(curr.right != null){
-                    q.offer(curr.right);
-                }
-            }
-
-            ans.add(level);
+            lsst.add(lst);
         }
 
-        return ans;
+        return lsst;
     }
 }
