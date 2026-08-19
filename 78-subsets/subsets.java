f1b90571c40cf8bd860c6befcc51729b44b194b0
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-08-20 04:46:13 +0530

    Time: 1 ms (86.47%) | Memory: 44.2 MB (39.45%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,23 +1,33 @@
+import java.util.*;
+
 class Solution {
+    List<List<Integer>> lst;
+
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> arr = new ArrayList<>();
-        ArrayList<Integer> a = new ArrayList<>();
-        arr.add(a);
+        lst = new ArrayList<>();
+        List<Integer> x = new ArrayList<>();
+        
+        // Seedhe index 0 se helper function call karein
+        bt(nums, x, 0);
+        return lst;
+    }
 
-        for (int i : nums) {
-            int n = arr.size();
-            for (int j = 0; j < n; j++) {
-                List<Integer> oldSubset = arr.get(j);
-                ArrayList<Integer> lst = new ArrayList<>();
-
-                for (int x : oldSubset) {
-                    lst.add(x);
-                }
-
-                lst.add(i);
-                arr.add(lst);
-            }
+    public void bt(int[] num, List<Integer> x, int idx) {
+        // Base Case: Jab saare elements check ho jayein
+        if (idx == num.length) {
+            // HAMESHA ek nayi copy banakar main list mein add karein
+            lst.add(new ArrayList<>(x));
+            return;
         }
-        return arr;
+
+        // Choice 1: Current element ko subset mein SHAMIL KAREIN (Take)
+        x.add(num[idx]);
+        bt(num, x, idx + 1);
+
+        // Backtracking Step: Agli choice ke liye element ko remove karein
+        x.remove(x.size() - 1);
+
+        // Choice 2: Current element ko CHHOD DEIN (Leave/Skip)
+        bt(num, x, idx + 1);
     }
 }
