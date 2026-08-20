4664e0bdb597cf55dbfeb9e20ef7dcfd71100274
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-08-20 19:27:02 +0530

    Time: 1 ms (87.45%) | Memory: 45.7 MB (15.64%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,28 +1,43 @@
 class Solution {
-    List<List<Integer>> lst ;
-    public List<List<Integer>> permute(int[] nums) {
-        lst = new ArrayList<>();
-        boolean[] u = new boolean[nums.length];
-        List<Integer> x = new ArrayList<>();
 
-        bt(nums ,x, u);
-        return lst;
+    public List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        backtrack(nums, 0, ans);
+
+        return new ArrayList<>(new HashSet<>(ans));
     }
-    public void bt(int[] nums , List<Integer> x ,boolean[] u){
-        if(x.size() == nums.length){
-            lst.add(new ArrayList<>(x));
+
+    public void backtrack(int[] nums, int idx, List<List<Integer>> ans) {
+
+        if (idx == nums.length) {
+
+            List<Integer> x = new ArrayList<>();
+
+            for (int num : nums) {
+                x.add(num);
+            }
+
+            ans.add(x);
+
             return;
         }
 
-        for(int i = 0 ; i < nums.length ; i++){
-            if(u[i]) continue;
-            x.add(nums[i]);
-            u[i] = true;
-            System.out.println("i am addidng to x " + nums[i]);
-            System.out.println("your x--> " + x);
-            bt(nums , x , u);
-            x.remove(x.size()-1);
-            u[i] = false;
+        for (int i = idx; i < nums.length; i++) {
+
+            // swap
+            int temp = nums[idx];
+            nums[idx] = nums[i];
+            nums[i] = temp;
+
+            // recursively continue swapping
+            backtrack(nums, idx + 1, ans);
+
+            // undo swap
+            temp = nums[idx];
+            nums[idx] = nums[i];
+            nums[i] = temp;
         }
     }
 }
