a8e401b30d6d6145788f97584068d51efeb57e0a
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-08-22 04:12:59 +0530

    Time: 2 ms (99.73%) | Memory: 45 MB (60.12%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    List<List<Integer>> lst ;
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+    // public List<List<Integer>> subsets(int[] nums) {
+        lst = new ArrayList<>();
+        // lst.add(new ArrayList<Integer>());
+        List<Integer> arr = new ArrayList<>();
+        // boolean[] u = new boolean[nums.length];
+        Arrays.sort(nums);
+        bt(nums , arr , 0);
+        // Collections.sort(lst);
+        // Collections.sort(lst, (a, b) -> Integer.compare(a.size(), b.size()));
+
+        return lst;
+    }
+    public void bt(int[] nums , List<Integer> arr, int idx){
+                lst.add(new ArrayList<>(arr));
+        if(idx > nums.length-1 ){
+            // lst.add(new ArrayList<Integer>(arr));
+            return ;
+        }
+        for(int i = idx ; i < nums.length ; i++){
+            if (i > idx && nums[i] == nums[i - 1])
+    continue;
+            // if(!u[i]){
+                arr.add(nums[i]);
+                bt(nums , arr , i+1 );
+                arr.remove(arr.size() -1 );
+                
+            // }
+        }
+    }
+}
