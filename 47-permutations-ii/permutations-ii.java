f9213bc4222f28e9bd1cb1420bc6cb927838bdbc
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-08-22 04:24:52 +0530

    Time: 36 ms (8.76%) | Memory: 53.6 MB (5.77%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    List<List<Integer>> ans;
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        ans = new ArrayList<>(); 
+        boolean[] used = new boolean[nums.length];
+        backtrack(nums, new ArrayList<>(), used);
+        Set<List<Integer>> set = new HashSet<>(ans);
+        ans = new ArrayList<>(set);
+        return ans;
+    }
+
+    public void backtrack(int[] nums, List<Integer> path, boolean[] used) {
+
+        if (path.size() == nums.length) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (used[i]) continue;
+
+            path.add(nums[i]);
+            used[i] = true;
+
+            backtrack(nums, path, used);
+
+            path.remove(path.size() - 1);
+            used[i] = false;
+        }
+    }
+}
