997780857e59c8a5a5ae5f75264f214cae86c98a
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-08-22 03:45:01 +0530

    Time: 1 ms (86.40%) | Memory: 44.4 MB (7.00%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,38 +1,27 @@
-import java.util.*;
-
 class Solution {
-    List<List<Integer>> lst;
-
+    List<List<Integer>> lst ;
     public List<List<Integer>> subsets(int[] nums) {
         lst = new ArrayList<>();
-        List<Integer> currentSubset = new ArrayList<>();
-        
-        // Index 0 se shuru karte hain
-        findSubsets(nums, 0, currentSubset);
+        lst.add(new ArrayList<Integer>());
+        List<Integer> arr = new ArrayList<>();
+        // boolean[] u = new boolean[nums.length];
+        bt(nums , arr , 0);
+        // Collections.sort(lst);
         return lst;
     }
-
-    public void findSubsets(int[] nums, int idx, List<Integer> current) {
-        // Base Case: Jab hum saare elements check kar chuke hon
-        if (idx == nums.length) {
-            // Is time hamare paas ek complete combination hai, isko main list mein save kar lo
-            lst.add(new ArrayList<>(current));
-            return;
+    public void bt(int[] nums , List<Integer> arr, int idx){
+        if(idx > nums.length-1 ){
+            // lst.add(new ArrayList<Integer>(arr));
+            return ;
         }
-
-        // --- OPTION 1: NOT TAKE (Element ko chhod dein) ---
-        // Hum current element ko touch nahi karte, seedhe agle index par chale jaate hain
-        findSubsets(nums, idx + 1, current);
-
-
-        // --- OPTION 2: TAKE (Element ko shamil karein) ---
-        // Pehle element ko list mein add karo
-        current.add(nums[idx]);
-        
-        // Ab is added element ke sath agle index par jao
-        findSubsets(nums, idx + 1, current);
-        
-        // Waapas aate waqt element ko hata do (Safai/Clean up step)
-        current.remove(current.size() - 1);
+        for(int i = idx ; i < nums.length ; i++){
+            // if(!u[i]){
+                arr.add(nums[i]);
+                lst.add(new ArrayList<>(arr));
+                bt(nums , arr , i+1 );
+                arr.remove(arr.size() -1 );
+                
+            // }
+        }
     }
 }
