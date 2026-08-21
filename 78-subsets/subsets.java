147f5f6294cd910e8d66d0d8f5f531d2322fc4d5
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-08-22 03:45:44 +0530

    Time: 2 ms (4.40%) | Memory: 43.9 MB (94.62%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -2,14 +2,17 @@ class Solution {
     List<List<Integer>> lst ;
     public List<List<Integer>> subsets(int[] nums) {
         lst = new ArrayList<>();
-        lst.add(new ArrayList<Integer>());
+        // lst.add(new ArrayList<Integer>());
         List<Integer> arr = new ArrayList<>();
         // boolean[] u = new boolean[nums.length];
         bt(nums , arr , 0);
         // Collections.sort(lst);
+        Collections.sort(lst, (a, b) -> Integer.compare(a.size(), b.size()));
+
         return lst;
     }
     public void bt(int[] nums , List<Integer> arr, int idx){
+                lst.add(new ArrayList<>(arr));
         if(idx > nums.length-1 ){
             // lst.add(new ArrayList<Integer>(arr));
             return ;
@@ -17,7 +20,6 @@ class Solution {
         for(int i = idx ; i < nums.length ; i++){
             // if(!u[i]){
                 arr.add(nums[i]);
-                lst.add(new ArrayList<>(arr));
                 bt(nums , arr , i+1 );
                 arr.remove(arr.size() -1 );
                 
