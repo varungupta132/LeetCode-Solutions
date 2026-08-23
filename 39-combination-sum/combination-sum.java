f849700db607365987e9330b5b45bc41077dd6bf
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-08-23 14:31:19 +0530

    Time: 3 ms (33.74%) | Memory: 46 MB (9.50%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -2,7 +2,9 @@ class Solution {
     List<List<Integer>> lst ;
     public List<List<Integer>> combinationSum(int[] ca, int ta) {
         lst = new ArrayList<>();
+        Arrays.sort(ca);
         bt(ca , ta , 0 , new ArrayList<>() );
+
         return lst ;
     }
     public void bt(int[] ca , int ta , int idx , ArrayList<Integer> arr ){
@@ -11,15 +13,18 @@ class Solution {
             lst.add(new ArrayList<>(arr));
             return;
         }
-        if(idx > ca.length-1 || ta < 0){
-            return ;
-        }
-        if(ca[idx] <= ta){
-            arr.add(ca[idx]);
-            bt(ca , ta-ca[idx] , idx , arr);
+        if(ca[idx] > ta) return;
+        for(int i = idx ; i < ca.length ; i++){
+            arr.add(ca[i]);
+            bt(ca , ta-ca[i] , i , arr);
             arr.remove(arr.size() -1);
         }
+        // if(idx > ca.length-1 || ta < 0){
+        //     return ;
+        // }
+        // if(ca[idx] <= ta){
+        // }
 
-        bt(ca , ta , idx+1, arr);
+        // bt(ca , ta , idx+1, arr);
     }
 }
