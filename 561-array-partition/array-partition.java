581e6f5322ec883caae23bcfdbd1df8118192937
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-08-23 16:28:45 +0530

    Time: 16 ms (97.73%) | Memory: 50 MB (5.02%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -3,7 +3,7 @@ class Solution {
         int sum = 0 ;
         Arrays.sort(nums);
         for(int i = nums.length -2 ; i >= 0 ; i = i - 2){
-            System.out.println("value of i is -> " + i + " nums[i] ==> " + nums[i]);
+            // System.out.println("value of i is -> " + i + " nums[i] ==> " + nums[i]);
             sum += nums[i];
         }
         return sum;
