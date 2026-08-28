768c4ce41c0ad1a38d60392d08cdcfa370d715d0
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-08-29 02:27:46 +0530

    Time: 0 ms (100.00%) | Memory: 110.9 MB (58.88%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -1,23 +1,22 @@
+import java.util.HashMap;
+
 class Solution {
     public int findCenter(int[][] edges) {
-        HashMap<Integer , Integer> hm = new HashMap<>();
-        int maxx = 0;
-        int ans = 0;
-        for(int[] x : edges){
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+        for (int[] x : edges) {
             int u = x[0];
             int v = x[1];
-            hm.put(u , hm.getOrDefault(v , 0) + 1);
-            hm.put(v , hm.getOrDefault(u , 0) + 1);
+            
+            // अपनी फ्रीक्वेंसी खुद बढ़ाएं (getOrDefault में सही वेरिएबल का उपयोग करें)
+            hm.put(u, hm.getOrDefault(u, 0) + 1);
+            hm.put(v, hm.getOrDefault(v, 0) + 1);
 
-            if(maxx < hm.getOrDefault(v , 0)){
-                maxx = hm.getOrDefault(v , 0);
-                ans = v;
-            }
-            else if(maxx < hm.getOrDefault(u , 0)){
-                maxx = hm.getOrDefault(u , 0);
-                ans = u;
-            }
+            // चूँकि सेंटर नोड हर एज में आता है, जैसे ही किसी नोड की 
+            // फ्रीक्वेंसी 2 होगी, वह पक्का सेंटर नोड ही होगा!
+            if (hm.get(u) > 1) return u;
+            if (hm.get(v) > 1) return v;
         }
-        return ans;
+        return 0;
     }
 }
