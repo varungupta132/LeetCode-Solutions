a2a947f37764e4a9ff072280fd971a492dad0aca
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-08-30 03:02:37 +0530

    Time: 492 ms (5.49%) | Memory: 54.7 MB (5.25%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.java
@@ -0,0 +1,36 @@
+class Solution {
+    boolean[][] u ;
+    public int numIslands(char[][] grid) {
+        u = new boolean[grid.length][grid[0].length];
+        int c = 0;
+        for(int i = 0 ; i < grid.length ; i++){
+            for(int j  = 0 ; j < grid[0].length ; j++){
+                if(grid[i][j] == '1' && !u[i][j] ){
+                    c++;
+                    dfs(grid , i , j);
+                }
+            }
+        }
+        return c;
+    }
+
+    public void dfs(char[][] grid ,int   i ,int   j ){
+        // System.out.println("calling dfs");
+        if( i < 0 || i > grid.length-1 || j < 0 || j  > grid[0].length -1 || grid[i][j] != '1' || u[i][j]) return;
+        // System.out.println(Arrays.deepToString(grid));
+        u[i][j] = true;
+        ArrayList<int[]>  arrr= new ArrayList<>(Arrays.asList(
+            new int[]{i+1 , j },
+            new int[]{i-1 , j },
+            new int[]{i , j+1 },
+            new int[]{i , j-1 }
+        ));
+        for(int[] x :  arrr ){
+            System.out.println("Doing somting");
+            dfs(grid , x[0], x[1]);
+        }
+
+
+
+    }
+}
