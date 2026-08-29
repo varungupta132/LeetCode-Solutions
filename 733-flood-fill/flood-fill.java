0245315c1b65d4edd956a86cbecc0d96ce1d31bd
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-08-29 19:38:51 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (43.35%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,40 +1,30 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        // boolean[][] u = new boolean[image.length][image[0].length];
-        
 
-        bfs(image , sr , sc , color , image[sr ][sc]);
+        int currColor = image[sr][sc];
 
-        return image ;
+        if(currColor == color)
+            return image;
+
+        dfs(image, sr, sc, currColor, color);
+
+        return image;
     }
-    public void bfs(int[][] img , int sr , int sc , int color ,int currcolor ){
-        if(sr < 0 || sr >= img.length || sc < 0 || sc >= img[0].length || img[sr][sc] == color || img[sr][sc] != currcolor ){
+
+    public void dfs(int[][] image, int r, int c,
+                    int currColor, int newColor) {
+
+        if(r < 0 || r >= image.length ||
+           c < 0 || c >= image[0].length ||
+           image[r][c] != currColor) {
             return;
         }
-        System.out.println(Arrays.deepToString(img));
-        img[sr][sc] = color;
-        // u[sr][sc] = true;
-        ArrayList<int[]> arr = new ArrayList<>(Arrays.asList(
-            new int[]{sr + 1, sc},
-            new int[]{sr - 1, sc},
-            new int[]{sr, sc + 1},
-            new int[]{sr, sc - 1}
-        ));
 
+        image[r][c] = newColor;
 
-        for(int[] x : arr){
-            int uu = x[0];
-            int v = x[1];
-            // if(img[uu][v] == color){
-            if(uu < 0 || uu >= img.length || v < 0 || v >= img[0].length ){
-                continue ;
-            }
-            if(img[uu][v] == currcolor)
-                bfs(img , uu , v , color , img[uu][v]);
-            // }
-        }
-
-        
-
+        dfs(image, r + 1, c, currColor, newColor); // down
+        dfs(image, r - 1, c, currColor, newColor); // up
+        dfs(image, r, c + 1, currColor, newColor); // right
+        dfs(image, r, c - 1, currColor, newColor); // left
     }
 }
