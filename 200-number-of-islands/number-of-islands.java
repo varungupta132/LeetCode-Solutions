d2092fb8d51d18a267df2a02df7d225c65585806
Varun Gupta <142798528+varungupta132@users.noreply.github.com>
2026-08-30 03:05:24 +0530

    Time: 3 ms (88.01%) | Memory: 52.1 MB (67.43%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,38 +1,43 @@
 class Solution {
-    boolean[][] u ;
+
     public int numIslands(char[][] grid) {
-        u = new boolean[grid.length][grid[0].length];
-        int c = 0;
-        for(int i = 0 ; i < grid.length ; i++){
-            for(int j  = 0 ; j < grid[0].length ; j++){
-                if(grid[i][j] == '1' && !u[i][j] ){
-                    c++;
-                    dfs(grid , i , j);
+
+        int count = 0;
+
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+
+                if(grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);
                 }
             }
         }
-        return c;
+
+        return count;
     }
 
-    public void dfs(char[][] grid ,int   i ,int   j ){
-        // System.out.println("calling dfs");
-        if( i < 0 || i > grid.length-1 || j < 0 || j  > grid[0].length -1 || grid[i][j] != '1' || u[i][j]) return;
-        // System.out.println(Arrays.deepToString(grid));
-        u[i][j] = true;
-        ArrayList<int[]>  arrr= new ArrayList<>(Arrays.asList(
-            new int[]{i+1 , j },
-            new int[]{i-1 , j },
-            new int[]{i , j+1 },
-            new int[]{i , j-1 }
-        ));
-        for(int[] x :  arrr ){
-            // if( x[0] < 0 || x[0] > grid.length-1 || x[1] < 0 || x[1]  > grid[0].length -1 || grid[x[0]][x[1]] != '1' || u[x[0]][x[1]]) continue ;
+    public void dfs(char[][] grid, int i, int j) {
 
-            // System.out.println("Doing somting");
-            dfs(grid , x[0], x[1]);
+        if(i < 0 || i >= grid.length ||
+           j < 0 || j >= grid[0].length ||
+           grid[i][j] != '1') {
+            return;
         }
 
+        // Mark visited
+        grid[i][j] = '0';
 
+        // Down
+        dfs(grid, i + 1, j);
 
+        // Up
+        dfs(grid, i - 1, j);
+
+        // Right
+        dfs(grid, i, j + 1);
+
+        // Left
+        dfs(grid, i, j - 1);
     }
 }
